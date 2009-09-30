3ab72928a179fe67d773fd2fab7773b1f99928da
Simon Harris <haruki.zaemon@gmail.com>
2009-09-30 14:04:16 +1000

    Done

--- lib/rack-pot/pot.rb
+++ lib/rack-pot/pot.rb
@@ -3,18 +3,18 @@ require 'rack'
 module Rack
 
   class Pot
+    
+    CONTENT_TYPE = "application/coffee-pot-command".freeze
 
     def initialize(app)
       @app = app
     end
 
     def call(env)
-      if env["REQUEST_METHOD"] == "BREW"
-        if env["CONTENT_TYPE"] == "application/coffee-pot-command"
-          [418, {"Content-Type" => "application/coffee-pot-command"}, []]
-        else
-          [406, {}, []]
-        end
+      if env["CONTENT_TYPE"] == CONTENT_TYPE
+        [418, {"Content-Type" => CONTENT_TYPE}, []]
+      else
+        @app.call(env)
       end
     end
 
--- spec/rack-pot/pot_spec.rb
+++ spec/rack-pot/pot_spec.rb
@@ -1,51 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Rack::Pot do
+  
+  def do_call(content_type, request_method, &block)
+    request = Rack::MockRequest.env_for("/", "CONTENT_TYPE" => content_type, :method => request_method)
+    @response = Rack::Pot.new(block).call(request)    
+  end
+  
+  describe "text/html" do
 
-  describe "BREW" do
+    %w{BREW POST GET PUT DELETE HEAD}.each do |method|
     
-    def perform_request(content_type)
-      app = lambda { |env| fail "Should not forward request for processing" }
-      request = Rack::MockRequest.env_for("/", :method => "BREW", "CONTENT_TYPE" => content_type)
-      Rack::Pot.new(app).call(request)
-    end
+      describe method do
+      
+        before do
+          do_call("text/html", method) { |env| :result  }
+        end
     
-    describe "Content-Type: application/coffee-pot-command" do
-      
-      it "returns 418 I'm a teapot" do
-        response = perform_request("application/coffee-pot-command")
-        response.should == [418, {"Content-Type" => "application/coffee-pot-command"}, []]
+        it "forwards the request for processing" do
+          @response.should == :result
+        end
+
       end
-
-    end
-
-    describe "Any other content type" do
-
-      it "returns 405 Method Not Allowed" do
-        response = perform_request("text/html")
-        response.should == [406, {}, []]
-      end
-      
-      it "returns a valid Accept header"
       
     end
     
   end
-  
-  describe "Other request methods" do
 
-    describe "Content-Type: application/coffee-pot-command" do
-
-    end
-
-    describe "Any other content type" do
-
-      it "passes the request on untouched"
+  describe "application/coffee-pot-command" do
+    
+    %w{BREW POST GET PUT DELETE HEAD}.each do |method|
+    
+      describe method do
       
-      it "returns a valid Accept header"
+        before do
+          do_call("application/coffee-pot-command", method) { |env| fail "Should not forward request for processing" }    
+        end
+    
+        it "returns 418 I'm a teapot" do
+          @response.should == [418, {"Content-Type" => "application/coffee-pot-command"}, []]
+        end
+
+      end
       
     end
-
+    
   end
 
 end
