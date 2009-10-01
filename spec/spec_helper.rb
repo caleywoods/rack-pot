d07d3088f0782a91366d252c9e6ca69738827145
Simon Harris <haruki.zaemon@gmail.com>
2009-10-01 12:05:46 +1000

    Ensure load path is independent of execution location

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'rack/mock'
 
 # Support running specs with "rake spec" and "spec"
-$LOAD_PATH.unshift("lib") unless $LOAD_PATH.include?("lib")
+lib = File.expand_path(File.dirname(__FILE__) + '/../lib')
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'rack-pot'
